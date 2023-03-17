2e1c2bdfbc9645d333e873372366b22d3bc2577e
SeragAmged <seragamged2002@gmail.com>
2023-03-18 01:59:38 +0200

    c for loop  practice6🔥🔥🔥

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
+
 int i;
+char l;
 for (i = 0; i < 10; i++)
 putchar(i + '0');
-char letter;
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
+
+for (l = 'a'; l <= 'f'; l++)
+putchar(l);
+
 putchar('\n');
 return (0);
 }
