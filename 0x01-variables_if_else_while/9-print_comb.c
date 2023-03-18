2546a76133c6290b521adba88b3136d2257852fd
SeragAmged <seragamged2002@gmail.com>
2023-03-18 02:13:54 +0200

    c for loop  practice7🔥🔥🔥

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@ int main(void)
 int i;
 for (i = 0; i < 10; i++)
 {
+
 putchar(i + '0');
-if (i != 9)
-putchar(' ');
+
 if (i != 9)
 putchar(',');
+
+if (i != 9)
+putchar(' ');
 
 }
 putchar('\n');
