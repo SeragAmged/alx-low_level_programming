678944475311dd7da465c78c2e5ea91d72a392a4
SeragAmged <seragamged2002@gmail.com>
2023-03-21 23:17:06 +0200

    fixt

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,12 +3,5 @@
 #include <stdio.h>
 
 char _putchar(char c);
-void print_alphabet(void){
-int i;
-for(i=97;i<=122;i++)
-{
-putchar((char)i);
-}
-putchar('\n');
-}
+void print_alphabet(void);
 #endif
