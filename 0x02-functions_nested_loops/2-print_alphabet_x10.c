cf243fffcea49a2d3951709e7bcb69881883325f
SeragAmged <seragamged2002@gmail.com>
2023-03-22 02:05:54 +0200

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase,
+ * followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+print_alphabet();
+}
