799dd10e36c0214b421120f353da21a70948e28a
SeragAmged <seragamged2002@gmail.com>
2023-03-22 02:09:44 +0200

    update🔥

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,12 @@
  */
 void print_alphabet_x10(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-print_alphabet();
+int j;
+char letter;
+for (j = 0; j < 9, j++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 }
