65e2075a726d5a5faeb07b1831d2950e339dd5a2
SeragAmged <seragamged2002@gmail.com>
2023-03-22 04:29:30 +0200

    fix var init🔧

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
  * @n: the input number.
  * Return:res digit of a number.
  */
-int print_res_digit(int n)
+int print_last_digit(int n)
 {
 long int m = n;
 int res;
@@ -14,5 +14,5 @@ if (m < 0)
 m = -m;
 res = m % 10;
 _putchar(res + '0');
-return (res); 
+return (res);
 }
