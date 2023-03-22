bba155f8ff88663d3e79ecee62da9215b546c2c0
SeragAmged <seragamged2002@gmail.com>
2023-03-22 04:49:39 +0200

    new sol

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,23 +6,17 @@
 
 void jack_bauer(void)
 {
-int i;
-int j;
+int i, j;
 for (i = 0; i < 24; i++)
 {
-
 for (j = 0; j < 60; j++)
 {
-if (i < 10)
-_putchar('0');
-_putchar(i + '0');
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 _putchar(':');
-if (j < 10)
-_putchar('0');
-_putchar(j + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
-
 }
-
 }
 }
