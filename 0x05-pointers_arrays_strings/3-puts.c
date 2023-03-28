4e5f3c1b7bcd5d91bc7d8c0fadef206a82071a3a
SeragAmged <seragamged2002@gmail.com>
2023-03-28 02:25:07 +0200

    print string and new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string.
+ * @str: the string.
+ * Return: 0 if success.
+ */
+int _puts(char *s)
+{
+int c = 0;
+
+for (; *s++;)
+putchar(&s);
+putchar('\n');
+return (c);
+}
