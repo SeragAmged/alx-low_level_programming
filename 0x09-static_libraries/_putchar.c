8b90ff2550552f5d015b659bc17086b0a5c291d1
SeragAmged <seragamged2002@gmail.com>
2023-04-11 01:31:52 +0200

    task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- *@c: The character to print
+* _putchar - writes the character c to stdout.
+*@c: The character to print.
 * Return: On success 1.
-* */
+*/
 
 int _putchar(char c)
 {
