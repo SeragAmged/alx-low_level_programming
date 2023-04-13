83c4dc963855ecdf198da1228fb2f81b6682d445
SeragAmged <seragamged2002@gmail.com>
2023-04-13 04:46:07 +0200

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+if (size == 0)
+return (NULL);
+
 char *arr = (char *)malloc(size * sizeof(char));
+if (arr == NULL)
+return (0);
+
 unsigned int i;
 for (i = 0; i < size; i++)
 {
